b440ceb4bd74cd5fa754b5546794349cdb20b215
gitJayesh <sharmajayesh372@gmail.com>
2023-01-23 18:59:42 +0530

    landing screen needs changes

--- client/src/Components/Screens/LandingScreenHome.js
+++ client/src/Components/Screens/LandingScreenHome.js
@@ -2,8 +2,27 @@ import React from "react";
 import { Container } from "react-bootstrap";
 const LandingScreenHome = () => {
   return (
-    <div className="bg-dark landing-page">
-      <Container></Container>
+    <div className="bg-dark landing-page place-center">
+      {/* <Container> */}
+      <div className="row py-lg-5  landing-content">
+        <div className="col-lg-6 col-md-8 mx-auto">
+          <p className="fw-bolder fs-4">Project Management System</p>
+          <p className="lead">
+            Something short and leading about the collection below—its contents,
+            the creator, etc. Make it short and sweet, but not too short so
+            folks don’t simply skip over it entirely.
+          </p>
+          <p>
+            <a href="#" class="btn btn-primary my-2 mx-2">
+              Main call to action
+            </a>
+            <a href="#" class="btn btn-secondary my-2">
+              Secondary action
+            </a>
+          </p>
+        </div>
+      </div>
+      {/* </Container> */}
     </div>
   );
 };
